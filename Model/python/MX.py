0cc880b0ff2909faf124933ed34bb6971ad2473c
藤野宏樹 <fujinohiroki@fujinohiroki-no-MacBook-Pro.local>
2016-01-13 01:06:35 +0900

    MX修正

--- Model/python/MX.py
+++ Model/python/MX.py
@@ -1,5 +1,9 @@
 # -*- coding:utf-8 -*-
-
+"""
+SVD++の実装
+参考文献:
+    「Advances in Collaborative Filtering」,Y.Koran and R.Bell ,2007 IEEE
+"""
 import numpy
 
 
@@ -15,7 +19,9 @@ def get_rating_error(r, p, q):
 @return(error) 誤差
 """
 def get_error(R, P, Q, beta):
+    
     error = 0.0
+    
     for i in range(len(R)):
         for j in xrange(len(R[i])):
             if R[i][j] == 0:
@@ -23,6 +29,7 @@ def get_error(R, P, Q, beta):
             error += pow(get_rating_error(R[i][j], P[:,i], Q[:,j]), 2)
     # 正規化項
     error += beta * (numpy.linalg.norm(P) + numpy.linalg.norm(Q))
+    
     return error
 
 """
@@ -36,8 +43,10 @@ def get_error(R, P, Q, beta):
 @return(P,Q) 最適化によって求められたユーザー行列Pとアイテム行列Q
 """
 def matrix_factorizaion(R, K, steps = 5000, gamma = 0.0002, beta = 0.02, threshold = 0.001):
+    
     P = numpy.random.rand(K, len(R))
     Q = numpy.random.rand(K, len(R[0]))
+    
     for step in xrange(steps):
         for i in xrange(len(R)):
             if R[i][j] == 0:
@@ -49,4 +58,5 @@ def matrix_factorizaion(R, K, steps = 5000, gamma = 0.0002, beta = 0.02, thresho
         error = get_error(R, P, Q, beta)
         if error < threshold:
             break
+    
     return P, Q
