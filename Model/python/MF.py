115bf2760932612b0e3bcb453b443145f8873f01
藤野宏樹 <fujinohiroki@fujinohiroki-no-MacBook-Pro.local>
2016-01-13 16:11:57 +0900

    MFをclassに変形

---
+++ Model/python/MF.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+"""
+SVD++の実装
+参考文献:
+    「Advances in Collaborative Filtering」,Y.Koran and R.Bell ,2007 IEEE
+"""
+import numpy
+
+class basicMF:
+    def __init__(self, R):
+        self.R = R # 評価値行列
+    
+    def get_rating_error(r, p, q):
+        return r- numpy.dot(p, q)
+
+    def get_error(self, beta):
+        """
+        目的関数
+        @param(beta) 正規化係数
+        """
+        self.error = 0.0
+
+        for user in range(len(self.R)):
+            for item in xrange(len(self.R[user])):
+                if R[user][item] == 0:
+                    continue
+                self.error += pow(self.get_rating_error(self.R[user][item], self.P[:,user], self.Q[:,item]), 2)
+            # 正規化項
+            self.error += beta * (numpy.linalg.norm(self.P) + numpy.linalg.norm(self.Q))
+    
+    def learning(self, K, steps = 5000, gamma = 0.0002, beta = 0.02, threshold = 0.001):
+        """
+        ユーザー行列Pとアイテム行列Qを最適化によって求める
+        @param(K) 疑似行列の次元
+        @param(steps) 学習回数
+        @param(alpha) 学習係数
+        @param(beta) 正規化係数
+        @param(threshold) 誤差の閾値
+        """
+        self.P = numpy.random.rand(K, len(self.R))
+        self.Q = numpy.random.rand(K, len(self.R[0]))
+
+        for step in xrange(steps):
+            for user in xrange(len(self.R)):
+                for item in xrange(len(self.R[user])):
+                    if self.R[user][item] == 0:
+                        continue
+                    err = self.get_rating_error(self.R[user][item], self.P[:,user], self.Q[:,item])
+                    for k in xrange(K):
+                        self.P[k][user] += gamma * (err*self.Q[k][item] - beta*self.P[k][user])
+                        self.Q[k][item] += gamma * (err*self.P[k][user] - beta*self.Q[k][item])
+            self.get_error(beta)
+            if self.error < threshold:
+                self.nR = numpy.dot(self.P.T, self.Q)
+                break
+
+    def predict(self, user, item):
+        return self.nR[user][item]
