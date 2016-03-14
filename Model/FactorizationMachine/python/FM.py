8e7b1709910b9b10aa159d13584ca642a9252fbf
藤野宏樹 <fujinohiroki@fujinohiroki-no-MacBook-Pro.local>
2016-03-14 22:37:24 +0900

    get_errorメソッド追加

--- Model/FactorizationMachine/python/FM.py
+++ Model/FactorizationMachine/python/FM.py
@@ -11,14 +11,29 @@ class FM:
     def __init__(self, R):
         self.R = R #評価値行列
 
-    def get_error(self):
+    def get_error(self, x_index, target):
         
-        return
+        # 各特徴量の重みの和
+        features = 0.0
+        # 相互作用の重みの和
+        iterations = 0.0
+        for i in xrange(len(self.R[0])):
+            features += self.W[i] * self.R[x_index][i]
+            for j in xrange(i+1, len(self.R[0])):
+                # 相互作用の特徴量の重みの和
+                interaction_parameters = 0.0
+                for f in xrange(K):
+                    interaction_parameters += self.V[i][f] * self.V[j][f]
+                iterations += interaction_parameters * self.R[x_index][i] * self.R[x_index][j]
+
+        self.error = (self.w_0 + features + iterations) - target
 
     def learning(self, K = 5):
         # バイアス
         self.w_0 = 0
         # 重み
-        self.W = np.random.rand(len(self.R[0]))
+        self.W = np.zeros(len(self.R[0]))
         # 相互作用の重み
         self.V = np.random.rand(len(self.R[0]),K)
+        # 誤差
+        self.error = 0
