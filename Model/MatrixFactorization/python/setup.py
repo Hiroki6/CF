cb73975a4c268871f966f4b9ca5171548b9ba4be
藤野弘樹 <fujinohiroki@fujinohiroki-no-MacBook-Pro.local>
2016-03-21 17:08:45 +0900

    コンパイル完了

--- Model/MatrixFactorization/python/setup.py
+++ Model/MatrixFactorization/python/setup.py
@@ -4,13 +4,10 @@ import numpy
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-"""
+Ext_modules = [Extension('cythonMF', ['cythonMF.pyx'], include_dirs=[numpy.get_include()])]
+
 setup(
+    name = 'cythonMF app',
     cmdclass = {'build_ext':build_ext},
-    ext_modules = [Extension("cythonMF", ["cythonMF.pyx"], include_dirs=[numpy.get_include()])]
-)
-"""
-setup(
-     ext_modules = cythonize("cythonMF.pyx"),
-     include_dirs = [numpy.get_include()]
+    ext_modules = Ext_modules
 )
