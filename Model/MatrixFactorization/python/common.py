ca76ab03d892d732d116f696dd33d6018b351373
藤野弘樹 <fujinohiroki@fujinohiroki-no-MacBook-Pro.local>
2016-03-24 21:37:21 +0900

    rate_matrix変更

--- Model/MatrixFactorization/python/common.py
+++ Model/MatrixFactorization/python/common.py
@@ -1,67 +1,22 @@
 # -*- coding:utf-8 -*-
 
-import pandas as pd
 import math
 import random
 import numpy as np
-from sklearn.feature_extraction import DictVectorizer
-
-def create_ratings():
-    
-    userlist = int_create_element("../../../data/ml-1m/users.dat")
-    itemlist = int_create_element("../../../data/ml-1m/movies.dat")
-    # userID::movieID::rating::timestamp
-    ratelist = create_ratelist("../../../data/ml-1m/ratings.dat")
-    prefs = create_prefs(userlist, ratelist)
-    rate_matrix = {}
-    for user in userlist:
-        rate_matrix.setdefault(user, {})
-        for item in itemlist:
-            if prefs[user].has_key(item):
-                rate_matrix[user][item] = prefs[user][item]
-            else:
-                rate_matrix[user][item] = 0
-
-    return rate_matrix
-
-def create_prefs(userlist, ratings):
-    
-    # prefs
-    prefs = {}
-    for user in userlist:
-        prefs.setdefault(user, {})
-
-    for rate in ratings:
-        prefs[int(rate[0])-1][int(rate[1])-1]=int(rate[2])
-
-    return prefs
 
 """
-int型
+@return(element_dic) {"値":id}の辞書
 """
-def int_create_element(filename):
+def create_element_map(filename):
 
-    ret = []
-    maxElement = 0
+    element_dic = {}
+    id = 0
     for line in open(filename):
-        now_element = int(line.replace("\n","").split('::')[0])
-        maxElement = now_element if now_element > maxElement else maxElement
+        element = line.replace("\n","").split('::')[0]
+        element_dic[element] = id
+        id += 1
 
-    for i in range(maxElement):
-        ret.append(i)
-    
-    return ret
-
-"""
-そのままの型1
-"""
-def str_create_element(filename):
-
-    ret = []
-    for line in open(filename):
-        ret.append(line.replace("\n","").split('::')[0])
-
-    return ret
+    return element_dic
 
 
 def create_ratelist(filename):
@@ -79,42 +34,43 @@ def create_ratelist(filename):
 @return(RateArray) テストデータを除いた教師データ
 @return(testData) キーにユーザーとアイテムを持つ300個のテスト用データディクショナリ
 """
-def create_test_data(RateArray):
+def create_test_data(rate_matrix):
   
-  testData = {}
+  testData = []
   count = 0
-  while(count < 300):
-      user = random.choice(RateArray.keys())
-      item = random.choice(RateArray[user].keys())
-      if RateArray[user][item] == 0:
+  while count < 300:
+      user = random.randint(0, len(rate_matrix)-1)
+      item = random.randint(0, len(rate_matrix[0])-1)
+      rate = rate_matrix[user][item]
+      if rate < 1.0:
           continue
-      if testData.has_key(user):
-          continue
-      testData.setdefault(user, {})
-      count+=1
-      testData[user][item] = RateArray[user][item]
-      RateArray[user][item] = 0
+      test = [user, item, rate]
+      testData.append(test)
+      rate_matrix[user][item] = 0
+      count += 1
 
-  return RateArray, testData
+  return rate_matrix, testData
 
 """
 numpy.array化する
 """
 def create_matrix():
 
-    userlist = str_create_element("../../../data/ml-1m/users.dat")
-    itemlist = str_create_element("../../../data/ml-1m/movies.dat")
+    usermap = create_element_map("../../../data/ml-1m/users.dat")
+    itemmap = create_element_map("../../../data/ml-1m/movies.dat")
     # userID::movieID::rating::timestamp
     ratelist = create_ratelist("../../../data/ml-1m/ratings.dat")
    
-    rate_matrix = pd.DataFrame(np.zeros((len(userlist), len(itemlist))), index= userlist, columns = itemlist)
+    rate_matrix = np.zeros((len(usermap),len(itemmap)))
     for rate in ratelist:
-        if rate[0] in rate_matrix and rate[1] in rate_matrix.columns:
-            rate_matrix[rate[0]][rate[1]] = int(rate[2])
+        user_id = usermap[rate[0]]
+        item_id = itemmap[rate[1]]
+        rate_matrix[user_id][item_id] = rate[2]
 
-    return rate_matrix
+    return rate_matrix, usermap, itemmap
 
 if __name__ == "__main__":
-    rate_matrix = create_matrix()
-    print rate_matrix
+    rate_matrix, usermap, itemmap = create_matrix()
+    rate_matrix, testData = create_test_data(rate_matrix)
+    print testData
 
